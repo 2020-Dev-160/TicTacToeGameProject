3063574e22570bf7b2d93afdc10e3754bffe8e2d
2020-Dev-160 <amritagolu678@gmail.com>
2020-09-29 15:10:57 +0530

    Extracted all static constant in a separate class GameConstant

--- src/main/java/com/kata/TictacToe/Game.java
+++ src/main/java/com/kata/TictacToe/Game.java
@@ -1,18 +1,10 @@
 package com.kata.TictacToe;
 
+import static com.kata.TictacToe.GameConstant.*;
+
 public class Game {
     public static final char PLAYER_X = 'X';
     public static final char PLAYER_O = 'O';
-
-    public static final int ZERO_POSITION = 0;
-    public static final int FIRST_POSITION = 1;
-    public static final int SECOND_POSITION = 2;
-    public static final int THIRD_POSITION = 3;
-    public static final int FOURTH_POSITION = 4;
-    public static final int FIFTH_POSITION = 5;
-    public static final int SIXTH_POSITION = 6;
-    public static final int SEVENTH_POSITION = 7;
-    public static final int EIGHTH_POSITION = 8;
     char previousPlayer;
     char[] board = new char[9];
 
