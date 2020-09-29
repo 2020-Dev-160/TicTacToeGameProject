a1cea57591a27432a669079358da5f4d611d9885
2020-Dev-160 <amritagolu678@gmail.com>
2020-09-29 13:51:15 +0530

    Extracted static variable for player X and O

--- src/main/java/com/kata/TictacToe/Game.java
+++ src/main/java/com/kata/TictacToe/Game.java
@@ -1,10 +1,13 @@
 package com.kata.TictacToe;
 
 public class Game {
+    public static final char PLAYER_X = 'X';
+    public static final char PLAYER_O = 'O';
+
     public char getPlayerAtPosition(int positionNumber) {
         if (positionNumber == 0)
-            return 'X';
+            return PLAYER_X;
         else
-            return 'O';
+            return PLAYER_O;
     }
 }
--- src/test/java/com/kata/TictacToeTest/GameTest.java
+++ src/test/java/com/kata/TictacToeTest/GameTest.java
@@ -7,7 +7,10 @@ import org.junit.Test;
 
 public class GameTest {
 
+    public static final char PLAYER_X = 'X';
+    public static final char PLAYER_O = 'O';
     private Game game;
+
     @Before
     public void init() {
         game = new Game();
@@ -15,10 +18,10 @@ public class GameTest {
 
     @Test
     public void gameShouldReturnXOnFirstTurn() {
-        Assert.assertEquals('X', game.getPlayerAtPosition(0));
+        Assert.assertEquals(PLAYER_X, game.getPlayerAtPosition(0));
     }
     @Test
     public void gameShouldReturnOonSecondTurn(){
-        Assert.assertEquals('O',game.getPlayerAtPosition(1));
+        Assert.assertEquals(PLAYER_O,game.getPlayerAtPosition(1));
     }
 }
